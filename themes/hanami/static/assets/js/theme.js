c9b8980cdf82430272975c19de6b2b3af1186322
Luca Guidi <me@lucaguidi.com>
2018-10-11 13:22:50 +0200

    Menu accordion

--- themes/hanami/static/assets/js/theme.js
+++ themes/hanami/static/assets/js/theme.js
@@ -1,8 +1,24 @@
 (function($){
   $(document).ready(function(){
+    // Alerts
     $("p.notice").prepend('<span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>');
     $("p.convention").prepend('<span class="alert-inner--icon"><i class="ni ni-ruler-pencil"></i></span>');
     $("p.warning").prepend('<span class="alert-inner--icon"><i class="ni ni-notification-70"></i></span>');
+
+    // Sidebar menu
+    var nonActiveMenuSections = $("ul.nav.ct-sidenav:not(:has(>.active))");
+    nonActiveMenuSections.hide()
+
+    $("span.ct-toc-link").click(function(event){
+      event.preventDefault();
+      var element = $(this);
+      var submenu = element.next("ul.nav.ct-sidenav");
+
+      nonActiveMenuSections.slideUp();
+      submenu.slideDown();
+    });
+
+    // Copy & Paste snippets
     $("div.highlight").before('<div class="ct-clipboard"><button class="btn-clipboard" title="" data-original-title="Copy to clipboard">Copy</button></div>');
   });
 })(jQuery);
