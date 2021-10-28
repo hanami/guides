f50f2aac12ef365b891f3ecf6721c292a0f1549c
Sebastian Wilgosz <sebastian@driggl.com>
2021-10-28 14:50:43 +0200

    Fix multi-section toggle
    Now after the section is clicked, it is slided down, while other non-active (url does not match) are slided-up.

--- themes/hanami/static/assets/js/theme.js
+++ themes/hanami/static/assets/js/theme.js
@@ -15,17 +15,36 @@
     var nonActiveMenuSections = $("ul.nav.ct-sidenav:not(:has(.active))");
     nonActiveMenuSections.hide()
 
-    $("span.ct-toc-link, li.ct-section-nav").click(function(event){
+    $("span.ct-toc-link").click(function(event){
       event.preventDefault();
       var element = $(this);
+
+      // Custom styles for currently opened link
+      $("span.ct-toc-link").removeClass('visible');
+      element.addClass('visible');
+
       var submenu = element.next("ul.nav.ct-sidenav");
 
-      console.log(submenu);
-
-      nonActiveMenuSections.slideUp();
+      $("ul.nav.ct-sidenav:not(:has(.active))").not(submenu).slideUp();
       submenu.slideDown();
     });
 
+    $("li.ct-section-nav span").click(function(event){
+      event.preventDefault();
+      var element = $(this).closest("li");
+
+      // Custom styles for currently opened link
+      $("li.ct-section-nav").removeClass('visible');
+      element.addClass('visible');
+
+      var submenu = element.children("ul.nav.ct-sidenav");
+
+      // close all other menus at the same deep-level
+      element.closest("ul").find("li ul.nav.ct-sidenav:not(:has(.active))").not(submenu).slideUp();
+
+      // slide-down the currently opened subsection
+      submenu.slideDown();
+    });
     // Copy & Paste snippets
     $("div.highlight").before('<div class="ct-clipboard"><button class="btn-clipboard" title="" data-original-title="Copy to clipboard">Copy</button></div>');
 
